61af32b5c166ac420b2fb3f8f3420a41aa991d08
casimirMorency <morencycasimir@gmail.com>
2022-08-21 22:20:02 -0400

    changed user icone and text for profile

--- src/components/user-panel-switcher-item/user-panel-profil.component.tsx
+++ src/components/user-panel-switcher-item/user-panel-profil.component.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { ConfigurableLink } from "@openmrs/esm-framework";
 import { Switcher } from "carbon-components-react";
-import UserAvatarFilledAlt24 from "@carbon/icons-react/es/user--avatar--filled--alt/24";
 import styles from "./user-panel-profil.component.scss";
 import { useTranslation } from "react-i18next";
+import { UserProfile16 } from "@carbon/icons-react";
 
 export default function AppointmentScheduling() {
   const { t } = useTranslation();
@@ -11,8 +11,8 @@ export default function AppointmentScheduling() {
     <div className={styles.Container}>
       <ConfigurableLink to="${openmrsBase}/spa/profil">
         <Switcher aria-label="Switcher Container">
-          <UserAvatarFilledAlt24 />
-          {t("profilChange")}
+          <UserProfile16 />
+          {t("profile")}
         </Switcher>
       </ConfigurableLink>
     </div>
